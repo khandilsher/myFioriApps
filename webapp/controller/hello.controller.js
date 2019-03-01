ef059633622ce94a0c1da32a1cef410f495c9aec
dilsher khan <khandilsher786@gmail.com>
2019-03-01 05:53:28 +0000

    init

---
+++ webapp/controller/hello.controller.js
@@ -0,0 +1,51 @@
+sap.ui.define([
+	"sap/ui/core/mvc/Controller",
+	"sap/m/MessageToast",
+	"sap/ui/model/json/JSONModel",
+	"sap/m/library",
+	"sap/ui/core/Locale",
+	"sap/ui/core/LocaleData",
+	"sap/ui/model/type/Currency"
+], function (Controller, MessageToast, JSONModel,mobileLibrary, Locale, LocaleData,Currency) {
+	"use strict";
+
+	return Controller.extend("demo3.DemoForBinding.controller.hello", {
+		formatMail: function(sFirstName, sLastName) {
+			var oBundle = this.getView().getModel("i18n").getResourceBundle();
+			return mobileLibrary.URLHelper.normalizeEmail(
+				sFirstName + "." + sLastName + "11@gmail.com",
+				oBundle.getText("mailSubject", [sFirstName]),
+				oBundle.getText("mailBody"));
+		},
+		formatStockValue: function(fUnitPrice, iStockLevel, sCurrCode) {
+			var sBrowserLocale = sap.ui.getCore().getConfiguration().getLanguage();
+			var oLocale = new Locale(sBrowserLocale);
+			var oLocaleData = new LocaleData(oLocale);
+			var oCurrency = new Currency(oLocaleData.mData.currencyFormat);
+			return oCurrency.formatValue([fUnitPrice * iStockLevel, sCurrCode], "string");
+		},
+		onItemSelected: function(oEvent) {
+			var oSelectedItem = oEvent.getSource();
+			var oContext = oSelectedItem.getBindingContext("Products");
+			var sPath = oContext.getPath();
+			var oProductDetailPanel = this.byId("productDetailsPanel");
+			oProductDetailPanel.bindElement({ path: sPath, model: "Products" });
+
+		},
+
+		/**
+		 * Called when a controller is instantiated and its View controls (if available) are already created.
+		 * Can be used to modify the View before it is displayed, to bind event handlers and do other one-time initialization.
+		 * @memberOf demo3.DemoForBinding.view.hello
+		 */
+		onInit: function () {
+			var oview = this.getView();
+			sap.ui.getCore().getMessageManager().registerObject(oview, true);
+		},
+		onShowHello: function () {
+			MessageToast.show("Hello World");
+		}
+
+	});
+
+});
---
+++ webapp/test/unit/AllTests.js
@@ -0,0 +1,5 @@
+sap.ui.define([
+	"demo3/DemoForBinding/test/unit/controller/View1.controller"
+], function () {
+	"use strict";
+});
